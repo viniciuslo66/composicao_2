5e7948155f63dc8fed6b2220f5c221703ce146a4
Vinicius Lourenco <viniciuslo66@gmail.com>
2020-01-03 15:52:39 -0300

    Feito entidades

--- src/Aplicacao/Programa.java
+++ src/Aplicacao/Programa.java
@@ -3,8 +3,7 @@ package Aplicacao;
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
 	}
-
 }
---
+++ src/Entidades/Comentarios.java
@@ -0,0 +1,21 @@
+package Entidades;
+
+public class Comentarios {
+	private String text;
+
+	public Comentarios() {
+	}
+
+	public Comentarios(String text) {
+		super();
+		this.text = text;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+}
