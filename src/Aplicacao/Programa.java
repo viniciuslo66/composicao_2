0f17734cf7005f4aaa81122bc2f865944e603af2
Vinicius Lourenco <viniciuslo66@gmail.com>
2020-01-06 14:51:22 -0300

    Aula finalizada

--- src/Aplicacao/Programa.java
+++ src/Aplicacao/Programa.java
@@ -1,9 +1,23 @@
 package Aplicacao;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import Entidades.Comentarios;
+import Entidades.Post;
+
 public class Programa {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		
+		 Comentarios c1 = new Comentarios("Have a nice trip!");
+		 Comentarios c2 = new Comentarios("Wow that's awesome");
+		 
+		 Post p1 = new Post(sdf.parse("21/06/2018 13:05:44"), "Viajando para Nova Zelandia", "Vou conhecer esse pais maravilhoso", 12);
+		 
+		 p1.adicionarComentario(c1);
+		 p1.adicionarComentario(c2);
 	}
 }
